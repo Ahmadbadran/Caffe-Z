7c313b4b3f3f751e20c78a3cc752332c50302b59
Ahmad Badran <ahmadbadran332211@gmail.com>
2023-03-15 17:19:35 +0300

    solved Q10 problem

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-<script type = "text/javascript">
-    var nickname = prompt("Please enter your name: ");
-    if (nickname != null) {
-      document.getElementById("welcome").innerText =
-      "Hello, " + nickname + "!"
-      }
+
+        let nickname = prompt("Please enter your name: ");
+        let gender = prompt("please select your gender: ");
+
+        if(nickname != null){
+
+        if(gender =="male"){
+
+            document.getElementById("home").innerText = "Hello, MR." + nickname + "!";
+            }
+            else if(gender == "female"){
+                document.getElementById("home").innerText = "Hello, Mrs." + nickname + "!";
+            }
+            else {
+                document.getElementById("home").innerText = "Hello, " + nickname + "!";
+            }
+        }
+        let temp = prompt("please choose what you wonna drink Hot or cold drink!!","hot");
+        let drink = prompt("Enter the name of the drink you like ... ");
+
+        alert("your drink " + drink + " is getting prepared !");
+
+        console.log("Hello " + nickname + " your order is "+ drink);
+
+        let num = prompt("enter a number berween 0 - 100");
+
+        for(i=1;num >100 || num<0;i++){
+             num = prompt("enter a number berween 0 - 100");
+
+        }
+        alert("the number you entered is " + num)
     
-  </script>
+        
+        
+    
