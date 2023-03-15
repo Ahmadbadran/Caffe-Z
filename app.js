fc43e05cfd79c3eb09088d2d9b8dc651720d607f
Ahmad Badran <ahmadbadran332211@gmail.com>
2023-03-15 16:41:07 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+<script type = "text/javascript">
+    var nickname = prompt("Please enter your name: ");
+    if (nickname != null) {
+      document.getElementById("welcome").innerText =
+      "Hello, " + nickname + "!"
+      }
+    
+  </script>
